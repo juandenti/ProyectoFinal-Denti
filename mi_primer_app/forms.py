3f9b560a6516af106640ae5b0029021026354508
Juan Denti <juandenti@hotmail.com>
2025-07-31 19:56:39 -0300

    Cambios

--- mi_primer_app/forms.py
+++ mi_primer_app/forms.py
@@ -1,16 +1,4 @@
 from django import forms
-
-
-class CursoForm(forms.Form):
-    nombre = forms.CharField(max_length=100, label='Nombre del Curso')
-    descripcion = forms.CharField(widget=forms.Textarea, label= 'Descripción')
-    duracion_semanas = forms.IntegerField(label= 'Duración (semanas)')
-    fecha_inicio = forms.DateField(widget=forms.SelectDateWidget, label='fecha de inicio')
-    fecha_fin = forms.DateField(widget=forms.SelectDateWidget, label='fecha de fin')
-    activo = forms.BooleanField(required=False, initial=True, label= 'Activo')
-
-    def __str__(self):
-        return self.nombre
     
 class PerfumeForm(forms.Form):
     nombre = forms.CharField(max_length=100, label='Nombre del Perfume')
@@ -19,12 +7,21 @@ class PerfumeForm(forms.Form):
     concentracion = forms.CharField(max_length=100, label='EDT/EDP')
     empresa = forms.CharField(max_length=100, label='Nombre de la Marca Creadora')
 
+    def __str__(self):
+        return self.nombre
+
 class MarcaForm(forms.Form):
     nombre = forms.CharField(max_length=100, label='Nombre de la marca')
     fecha_fundada = forms.DateField(widget=forms.SelectDateWidget, label='Fecha de fundación')
     activo = forms.BooleanField(required=False, initial=True, label= 'Activo')
 
+    def __str__(self):
+        return self.nombre
+
 class VelaForm(forms.Form):
     nombre = forms.CharField(max_length=100, label='Nombre de la vela aromática')
     tipo_de_aroma = forms.CharField(widget=forms.Textarea, label= 'Tipo de aroma')
     empresa = forms.CharField(max_length=100, label='Nombre de la Marca Creadora')
+
+    def __str__(self):
+        return self.nombre
--- mi_primer_app/urls.py
+++ mi_primer_app/urls.py
@@ -4,11 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    # path('hola-mundo/', views.hola_mundo, name='hola_mundo'),
-    # path('crear-curso/', views.crear_curso , name='crear-curso'),
-    # path('listar-cursos/', views.listar_cursos, name="listar-cursos"),
-    # path('cursos/buscar/', views.buscar_cursos, name="buscar-cursos"),
-
     path('cargar-perfume/', views.cargar_perfume, name='cargar-perfume'),
     path('cargar-marca/', views.cargar_marca, name='cargar-marca'),
     path('cargar-vela/', views.cargar_vela, name='cargar-vela'),
